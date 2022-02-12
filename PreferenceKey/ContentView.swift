942c7d83e6754e946e3f4dc67198a33fa74d1cfd
Matthew Cox <macox@ancestry.com>
2022-02-11 21:09:14 -0700

    Initial Commit

---
+++ PreferenceKey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PreferenceKey
+//
+//  Created by Matthew Cox on 2/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PreferenceKey/PreferenceKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PreferenceKeyApp.swift
+//  PreferenceKey
+//
+//  Created by Matthew Cox on 2/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PreferenceKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
